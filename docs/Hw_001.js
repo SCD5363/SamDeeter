120904d1611781bbf1bedb6e2d44b4a267daf887
SamDeeter <scd5363@psu.edu>
2022-11-09 13:00:10 -0500

    CleanUp2

---
+++ docs/Hw_001.js
@@ -0,0 +1,25 @@
+window.addEventListener('DOMContentLoaded',init,false);
+            
+function init() {
+    alert('The page loaded!');
+}var buttons = document.getElementsByTagName("button")
+
+function init() {
+    alert ('Hi there! Looks like the page loaded! Yay!');
+    var buttons = document.getElementsByTagName("button")
+buttons[0].addEventListener('click', changeColor,false)
+buttons[1].addEventListener('click', newFunction, false)
+}
+
+function changeColor() {
+var colorMe1 = document.getElementById("colorToggle") 
+{colorMe1.style.backgroundColor = "skyblue";
+}
+/* here, style is a *property*: the CSS styling of an element: you can add a CSS property after invoking style. */
+{Design.style.link = "AboutStyleSheet.css"}
+function newFunction() { 
+Messaging ('hi there')
+}var buttons = document.getElementsByTagName("button")
+/* We haven't written what is supposed to happen here yet. But this is an example of a JavaScript comment. */
+
+}
